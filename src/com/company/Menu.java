860af9d748e725bee8f9cb685dcec90e782f4368
dam2a <fdomin@elpuig.xeill.net>
2019-10-29 16:27:17 +0100

    dfdsf

---
+++ src/com/company/Menu.java
@@ -0,0 +1,40 @@
+package com.company;
+
+
+
+public class Menu {
+    void lanzarMenu(){
+
+
+
+
+        System.out.println("TORNEO: ");
+        System.out.println("1.Equipos");
+        System.out.println("2.Participantes");
+        System.out.println("3.Resultados");
+        System.out.println("4.Clasificacion");
+        System.out.println("5. Salir");
+        System.out.println("Elige una opcion:");
+
+        int opcion = sc.nextInt();
+
+        switch (opcion) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+                return;
+
+        }
+    }
+    }
+}
