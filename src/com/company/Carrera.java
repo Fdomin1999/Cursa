d0e9f4191f4fb50c6a3ca86977e8d55a830672d6
Dani <dalvarezr95@elpuig.xeill.net>
2019-11-06 18:45:24 +0100

    hg,

--- src/com/company/Carrera.java
+++ src/com/company/Carrera.java
@@ -23,28 +23,53 @@ public class Carrera {
 
 
     void resultado(){
-        if (tipoVehiculo.equals("coche")){
-            System.out.println(nombre + " " + "1" + ":" + (random.nextInt(15) + 30) + " Minutos");
-            for (int i = 0; i < cantidadParticipantes-1; i++) {
+        for (int j = 0; j < cantidadCircuitos; j++) {
+            System.out.println("Circuito " + (j+1));
 
-                System.out.println("Paricipante " + (i+1) + " " + "1" + ":" + (random.nextInt(15) + 30) + " Minutos");
 
+            if (tipoVehiculo.equals("coche")) {
+                System.out.println(nombre + " " + "1" + ":" + (random.nextInt(15) + 30) + " Minutos");
+                for (int i = 0; i < cantidadParticipantes - 1; i++) {
+
+                    System.out.println("Participante " + (i + 1) + " " + "1" + ":" + (random.nextInt(15) + 30) + " Minutos");
+
+
+                }
+
+            } else if (tipoVehiculo.equals("bicicleta")) {
+                System.out.println(nombre + " " + (random.nextInt(5) + 5) + ":" + (random.nextInt(15) + 30) + " Minutos");
+                for (int i = 0; i < cantidadParticipantes - 1; i++) {
+
+                    System.out.println("Participante " + (i + 1) + (random.nextInt(5) + 5) + ":" + (random.nextInt(15) + 30) + " Minutos");
+
+
+                }
+
+
+            } else if (tipoVehiculo.equals("moto")) {
+
+                System.out.println(nombre + " " + "1" + ":" + (random.nextInt(29) + 30) + " Minutos");
+                for (int i = 0; i < cantidadParticipantes - 1; i++) {
+
+                    System.out.println("Participante " + (i + 1) + " " + "1" + ":" + (random.nextInt(29) + 30) + " Minutos");
+
+
+                }
+
+
+            } else if (tipoVehiculo.equals("camion")) {
+
+                System.out.println(nombre + " " + (random.nextInt(8) + 5) + ":" + (random.nextInt(15) + 30) + " Minutos");
+                for (int i = 0; i < cantidadParticipantes - 1; i++) {
+
+                    System.out.println("Participante " + (i + 1) + (random.nextInt(8) + 5) + ":" + (random.nextInt(15) + 30) + " Minutos");
+
+
+                }
 
             }
 
-        } else if (tipoVehiculo.equals("bicicleta")){
-            System.out.println((random.nextInt(5)+5) + ":" +  (random.nextInt(15)+30) + " Minutos");
-
-
-
-        } else if (tipoVehiculo.equals("moto")){
-
-            System.out.println("1" + ":" +  (random.nextInt(29)+30) + " Minutos");
-
-
-        } else if (tipoVehiculo.equals("camion")){
-
-            System.out.println((random.nextInt(8)+5) + ":" +  (random.nextInt(15)+30) + " Minutos");
+            System.out.println("\n");
 
 
         }
